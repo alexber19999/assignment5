75a4babbd3fc6ccb26ff2ad9ef07a41b01644232
Alexander Berezovsky <alexber1999@my.uri.edu>
2019-04-16 13:02:38 -0400

    started working on checkNearestNeighbors

--- main.cpp
+++ main.cpp
@@ -8,7 +8,64 @@
  * */
 
 std::vector<int> checkNearestNeighbors(std::vector<std::vector<float> > grid, std::vector<int> position){
+    int rowNumber = position[0];
+    int colNumber = position[1];
 
+    std::vector<int> newPos;
+
+    if(rowNumber == 0 && colNumber == 0){
+        int right = grid[rowNumber][colNumber + 1];
+        int below = grid[rowNumber + 1][colNumber];
+        int diag = grid[rowNumber + 1][colNumber + 1];
+
+        int minimum = std::min({right, below, diag});
+
+        if(minimum == right){
+            newPos.push_back(rowNumber);
+            newPos.push_back(colNumber + 1);
+        } else if(minimum == below){
+            newPos.push_back(rowNumber + 1);
+            newPos.push_back(colNumber);
+        } else{
+            newPos.push_back(rowNumber + 1);
+            newPos.push_back(colNumber + 1);
+        }
+
+    } else if(rowNumber == 0 && colNumber == grid[0].size() - 1){
+        int left = grid[rowNumber][colNumber  - 1];
+        int below = grid[rowNumber + 1][colNumber];
+        int diag = grid[rowNumber + 1][colNumber - 1];
+
+        int minimum = std::min({left, below, diag});
+
+        if(minimum == left){
+            newPos.push_back(rowNumber);
+            newPos.push_back(colNumber - 1);
+        } else if(minimum == below){
+            newPos.push_back(rowNumber + 1);
+            newPos.push_back(colNumber);
+        } else{
+            newPos.push_back(rowNumber + 1);
+            newPos.push_back(colNumber - 1);
+        }
+
+    } else if(rowNumber == 0 && colNumber != grid[0].size() - 1){
+
+    } else if(rowNumber != grid.size() - 1 && colNumber == 0){
+
+    } else if(rowNumber != grid.size() - 1 && colNumber != grid[0].size() - 1){
+
+    } else if(rowNumber != grid.size() - 1 && colNumber == grid[0].size() - 1){
+
+    } else if(rowNumber == grid.size() - 1 && colNumber == 0){
+
+    } else if(rowNumber == grid.size() - 1 && colNumber != grid[0].size() - 1){
+
+    } else if(rowNumber == grid.size() - 1 && colNumber == grid[0].size() - 1){
+
+    }
+
+    return newPos;
 }
 
 std::vector<int> getLocalMinimum(std::vector<std::vector<float> > grid, std::vector<int> position){
