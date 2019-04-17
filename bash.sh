6e04030f9c6d14701b509689a28d8fe87f53a153
Alexander <alexber1999@my.uri.edu>
2019-04-16 22:55:28 -0400

    added forking and writing to file

--- bash.sh
+++ bash.sh
@@ -1,1 +1,2 @@
 #!/usr/bin/env bash
+./main $1 $2 $3 $4
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include <fstream>
+#include <unistd.h>
 #include "Grid.h"
 
 
@@ -19,7 +23,7 @@ std::vector<std::vector<int> > getNearestNeighbors(std::vector<std::vector<float
     std::vector<std::vector<int> > newGrid;
 
     if(rowNumber == 0 && colNumber == 0){
-        int numElems = 0;
+//        int numElems = 0;
         for(int i = rowNumber; i < rowNumber + 2; i++){
             std::vector<int> row;
             for(int j = colNumber; j < colNumber + 2; j++){
@@ -27,12 +31,12 @@ std::vector<std::vector<int> > getNearestNeighbors(std::vector<std::vector<float
                 row.push_back(j);
                 newGrid.push_back(row);
                 row.clear();
-                numElems += 2;
+//                numElems += 2;
             }
 
         }
 
-        std::cout << "NUMBER OF ELEMENTS: " << numElems << std::endl;
+//        std::cout << "NUMBER OF ELEMENTS: " << numElems << std::endl;
 
     } else if(rowNumber == 0 && colNumber == grid[0].size() - 1){
         for(int i = rowNumber; i < rowNumber + 2; i++){
@@ -152,12 +156,12 @@ std::vector<int> checkNearestNeighbors(std::vector<std::vector<float> > grid, st
 
     std::vector<std::vector<int> > newGrid = getNearestNeighbors(grid, position);
 
-
-    std::cout << position[0] << " : " << position[1] << std::endl;
-    std::cout << newGrid.size() << std::endl;
-    std::cout << newGrid[0].size() << std::endl;
-    std::cout << newGrid[1].size() << std::endl;
-    std::cout << std::endl;
+//
+//    std::cout << position[0] << " : " << position[1] << std::endl;
+//    std::cout << newGrid.size() << std::endl;
+//    std::cout << newGrid[0].size() << std::endl;
+//    std::cout << newGrid[1].size() << std::endl;
+//    std::cout << std::endl;
 
     newPos = getLowestPoint(newGrid, grid);
 
@@ -175,9 +179,18 @@ std::vector<int> getLocalMinimum(std::vector<std::vector<float> > grid, std::vec
 
 
 int main(int argc, char *argv[]){
-    int numSkiers = std::atoi(argv[1]);
+    if(argc != 5){
+        std::cout << "Invalid number of arguments" << std::endl;
+        exit(-1);
+    }
+    std::ofstream outfile("results.txt");
+    int PPID = getpid();
+    srand(time(NULL));
     Grid grid;
     std::string file = argv[1];
+    int numSkiers = std::atoi(argv[2]);
+    int numRuns = std::atoi(argv[3]);
+    bool traceMode = argv[4];
     std::vector <std::vector<float> > fileGrid = grid.parseFile(file);
 //    for(int i = 0; i < fileGrid.size(); i++){
 //        for(int j = 0; j < fileGrid[i].size(); j++){
@@ -186,26 +199,55 @@ int main(int argc, char *argv[]){
 //        std::cout << std::endl;
 //
 //    }
-    std::vector<int> testPos;
-    testPos.push_back(3);
-    testPos.push_back(1);
+//    std::vector<int> testPos;
+//    testPos.push_back(3);
+//    testPos.push_back(1);
+//
+////    std::vector<int> minimum = getLocalMinimum(fileGrid, testPos);
+////
+////    std::cout << minimum[0] << std::endl;
+////    std::cout << minimum[1] << std::endl;
+////    std::cout << fileGrid[minimum[0]][minimum[1]] << std::endl;
+//
+//    std::vector<std::vector<int> > smallGrid = getNearestNeighbors(fileGrid, minimum);
+////
+////    std::cout << std::endl;
+////
+////    for(int i = 0; i < smallGrid.size(); i++){
+////        for(int j = 0; j < smallGrid[i].size(); j++){
+////            std::cout << smallGrid[i][j] << " " << std::endl;
+////        }
+////        std::cout << std::endl;
+////    }
+////
 
-    std::vector<int> minimum = getLocalMinimum(fileGrid, testPos);
-
-    std::cout << minimum[0] << std::endl;
-    std::cout << minimum[1] << std::endl;
-    std::cout << fileGrid[minimum[0]][minimum[1]] << std::endl;
-
-    std::vector<std::vector<int> > smallGrid = getNearestNeighbors(fileGrid, minimum);
-
-    std::cout << std::endl;
-
-    for(int i = 0; i < smallGrid.size(); i++){
-        for(int j = 0; j < smallGrid[i].size(); j++){
-            std::cout << smallGrid[i][j] << " " << std::endl;
+    int numChildren = 0;
+    int p = PPID;
+    for(int i = 0; i < numSkiers && getpid() == PPID; i++){
+        int randomRow = rand() % fileGrid.size();
+        int randomCol = rand() % fileGrid[0].size();
+        numChildren++;
+        p = fork();
+        if(p == 0){
+            std::cout << "HERE" << std::endl;
+            std::vector<int> start = {randomRow, randomCol};
+            std::vector<int> min = getLocalMinimum(fileGrid, start);
+            outfile << "Local Minimum: ";
+            outfile << min[0] << ", ";
+            outfile << min[1] << "\n";
+            sleep(1);
+        }else if(p < 0){
+            std::cout << "Creation Failed" << std::endl;
+            exit(-1);
         }
-        std::cout << std::endl;
     }
+
+    if(p != 0){
+        outfile.close();
+        std::cout << "CHILDREN: " << numChildren << std::endl;
+    }
+
+
 
 
 
